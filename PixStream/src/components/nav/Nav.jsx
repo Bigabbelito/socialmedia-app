b9f437227055e95baf8d0e7037a7fa9f9ece236c
Abdulah Abou Shihab <118314337+Bigabbelito@users.noreply.github.com>
2024-03-15 13:49:29 +0300

    dark-mode och lite design är klar

--- PixStream/src/components/nav/Nav.jsx
+++ PixStream/src/components/nav/Nav.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import './nav.css'
+import { Link } from 'react-router-dom'
+import DarkMode from '../darkmode/DarkMode'
+import CurrentUser from '../../FackApis/CurrentUserData'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faBell, faEnvelope, faHome, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+
+
+
 
 export default function Nav() {
   return (
-    <div>
-    Nav
+    <nav>
+    <div className='nav-container'>
+     <div className='nav-left'>
+      <Link to='/'>
+      <h3 className='logo'>PixStream </h3>
+      </Link>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faHome}/>
+      </Link>
+      <Link to='/profile/:id'>
+      <FontAwesomeIcon icon={faUser} />
+      </Link>
+      <div className='Nav-Searchbar'>
+      <FontAwesomeIcon icon={faSearch} />
+      <input type='search'/>
+      </div>
+     </div>
+
+     <div className='nav-right'>
+      <Link to='/chatbox/:id'>
+      <FontAwesomeIcon icon={faEnvelope} />
+      </Link>
+      <Link to='/chatbox/:id'>
+      <FontAwesomeIcon icon={faBell} />
+      </Link>
+      <DarkMode />
+      <Link to='/chatbox/:id'>
+      <FontAwesomeIcon icon={faBars} />
+      </Link>
+      <div className='user'>
+      <img src={CurrentUser.map(user=>(user.ProfieImage))} alt='' />
+      <h4>Abdulah Abou Shihab</h4>
+      </div>
+
+     </div>
+     
+
     </div>
+    </nav>
   )
 }
